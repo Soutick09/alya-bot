62d336f0abd602c5c90949676333aaeb203cd2c9
Soutick09 <soutickadak99@gmail.com>
2024-10-23 08:11:05 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator: <a href='tg://user?id={OWNER_ID}'>Soutick</a>\n○ Main Channel: <a href='https://t.me/STK_Animes'>STK Animes</a>\n○ Best Friend: <a href='tg://settings'>This Person</a>\n○ Adult Channel: <a href='https://t.me/AIO_Backup'>AIO_Backup 🔞</a></b>",
+            text = f"<b>○ Creator: <a href='tg://user?id={OWNER_ID}'>Soutick</a>\n○ Main Channel: <a href='https://t.me/STK_Animes'>STK Animes</a>\n○ Best Friend: <a href='tg://settings'>This Person</a>\n○ Adult Channel: <a href='https://t.me/AIO_Backup'>AIO Backup 🔞</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
